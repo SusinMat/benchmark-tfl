736f2dbea8da2a3878700d3ba5982f823ba33651
SusinMat <57694766+SusinMat@users.noreply.github.com>
2018-08-10 15:33:11 +0000

    Add pthreads

--- bumblebee/bumblebee.c
+++ bumblebee/bumblebee.c
@@ -1,6 +1,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -9,15 +11,32 @@
 #include <time.h>
 #include <unistd.h>
 
+#define MILLION 1000000
+#define BILLION 1000000000
+
+sem_t sem_r, sem_w;
+struct timespec time_shared;
+char *buffer_shared;
+
 int64_t get_time()
 {
 	struct timespec time;
 	int64_t ms = 0;
 
 	clock_gettime(CLOCK_MONOTONIC, &time);
-	ms = time.tv_sec * 1000 + time.tv_nsec / 1000000;
+	ms = time.tv_sec * 1000 + time.tv_nsec / MILLION;
 
 	return ms;
+}
+
+void *writer_thread(void *v)
+{
+	while (true) {
+		sem_wait(&sem_w);
+		printf("MONOTONIC_CLOCK: %010ld.%03ld s\n", time_shared.tv_sec, time_shared.tv_nsec / MILLION);
+		printf("%s\n", buffer_shared); // TODO: Use 2 buffers, alternating
+		// sem_post(&sem_r); // TODO: Remove this
+	}
 }
 
 int set_interface_attribs(int fd, int speed, int parity)
@@ -79,11 +98,14 @@ int main(int argc, char **argv)
 	const int record_size = 7;
 	const int record_max = 1000;
 	const int buf_size = record_max * record_size;
-	char buf[buf_size];
+	char buf[buf_size]; // TODO: Use 2 buffers, alternating
+	buffer_shared = buf; // TODO: Remove this
 	int cursor_position = 0;
 	char *portname = "/dev/ttyUSB0";
 	int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
 	struct timespec time;
+	pthread_t writer;
+
 	// printf("      This      is\n""<B><U><M><B><L><E><B><E><E>\n");
 	if (fd < 0) {
 		printf("error %d opening %s: %s\n", errno, portname, strerror(errno));
@@ -97,10 +119,13 @@ int main(int argc, char **argv)
 	// usleep((7 + 25) * 100);                  // sleep enough to transmit the 7 plus
 	// receive 25:  approx 100 uS per char transmit
 
-	do {
-		clock_gettime(CLOCK_MONOTONIC, &time);
-		printf("MONOTONIC_CLOCK: %010ld.%03ld s\n", time.tv_sec, time.tv_nsec / 1000000);
-		// printf("MONOTONIC_CLOCK: %"PRId64" ms\n", get_time());
+	sem_init(&sem_w, 0, 0);
+	sem_init(&sem_r, 0, 1); // TODO: remove this
+	pthread_create(&writer, NULL, writer_thread, NULL);
+
+	for (;;) {
+		clock_gettime(CLOCK_MONOTONIC, &time_shared);
+		// printf("MONOTONIC_CLOCK: %010ld.%03ld s\n", time.tv_sec, time.tv_nsec / MILLION);
 		for (cursor_position = 0; cursor_position < buf_size - record_size * 2; cursor_position += record_size) {
 			int bytes_read = 0;
 			while (bytes_read < record_size) {
@@ -110,8 +135,10 @@ int main(int argc, char **argv)
 			}
 		}
 		buf[cursor_position] = '\0';
+		// sem_wait(&sem_r); // TODO: remove this
+		sem_post(&sem_w);
 		printf("%s\n", buf);
-	} while (true);
+	}
 
 	return 0;
 }
