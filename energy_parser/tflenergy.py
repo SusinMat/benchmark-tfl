37c8a20ee2ddecaaf232153c0055163b125f7419
SusinMat <57694766+SusinMat@users.noreply.github.com>
2018-08-14 20:56:20 -0300

    Add output to energy_parser

--- energy_parser/tflenergy.py
+++ energy_parser/tflenergy.py
@@ -12,20 +12,21 @@ def s_to_ms(s):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 tfenergy <input_file_name>")
+        print("Usage: python3 tfenergy.py <input_file_name>")
         exit(1)
 
     filename = sys.argv[1]
 
-    input_file = [line[:-1] for line in open(filename, "r").readlines()]
+    input_file = [line[:-1] for line in open(filename, "r").readlines()] # we strip the last character of each line, typically a \n
 
     if len(input_file) < 6:
+        # input file must have at least one record other than the first and last
         print("Error: input file is only " + str(len(input_file)) + " lines long.")
         exit(1)
 
-    input_file = input_file[2:-2]
+    input_file = input_file[2:-2] # we strip the first and last records
 
-    if len(input_file) % 2 != 0:
+    if len(input_file) % 2 != 0: # there must be an even number of lines (timestamp followed by readings)
         print("Error: input file has an odd number of lines.")
         exit(1)
 
@@ -46,7 +47,10 @@ if __name__ == "__main__":
         timestamp = s_to_ms(clock_pattern.match(clock).group("clock"))
         readings = [float(r) for r in readings.split(",")]
         timestamp_offset = timestamp - previous_timestamp
-        print(str(timestamp) + " - " + str(previous_timestamp) + " = " + str(timestamp_offset))
-        power += [float('NaN') for i in range(len(readings) + timestamp_offset)]
+        print(str(timestamp) + " - " + str(previous_timestamp) + " = " + str(timestamp_offset)) # typically 1~2 ms
+        # power += [float('NaN') for i in range(len(readings) + timestamp_offset)]
+        power += [voltage * r for r in readings]
         previous_timestamp = timestamp + len(readings)
+    for i in range(len(power)):
+        print(str(starting_timestamp + i) + " ms, " + "%.03f" % (power[i]) + " W")
 
